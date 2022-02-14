656a575e978c53598e412fbc3cc358940c140393
Aroyan <abssneakers@gmail.com>
2022-02-14 23:12:21 +0700

    Playing around with useEffect hooks

--- src/components/Effect.js
+++ src/components/Effect.js
@@ -30,6 +30,21 @@ export default function Effect() {
     'KeyX',
     'KeyY',
     'KeyZ',
+    'Enter',
+    'ShiftLeft',
+    'AltLeft',
+    'ControlLeft',
+    'Tab',
+    'Digit1',
+    'Digit2',
+    'Digit3',
+    'Digit4',
+    'Digit5',
+    'Digit6',
+    'Digit7',
+    'Digit8',
+    'Digit9',
+    'Digit0',
   ];
 
   const handleKeyPress = (e) => {
@@ -42,6 +57,10 @@ export default function Effect() {
 
   useEffect(() => {
     document.addEventListener('keypress', handleKeyPress);
+  });
+
+  useEffect(() => {
+    document.title = `${keyCode}`;
   });
 
   return (
