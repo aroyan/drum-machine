1474ef96228a12040721ccfb63a178c4032b7d77
Aroyan <abssneakers@gmail.com>
2022-02-14 23:03:40 +0700

    Listening to keypress event using useEffect Hooks

--- src/components/Effect.js
+++ src/components/Effect.js
@@ -2,15 +2,53 @@ import React, { useState, useEffect } from 'react';
 
 export default function Effect() {
   const [count, setCount] = useState(0);
+  const [keyCode, setKeyCode] = useState('');
+  const keyCodes = [
+    'KeyA',
+    'KeyB',
+    'KeyC',
+    'KeyD',
+    'KeyE',
+    'KeyF',
+    'KeyG',
+    'KeyH',
+    'KeyI',
+    'KeyJ',
+    'KeyK',
+    'KeyL',
+    'KeyM',
+    'KeyN',
+    'KeyO',
+    'KeyP',
+    'KeyQ',
+    'KeyR',
+    'KeyS',
+    'KeyT',
+    'KeyU',
+    'KeyV',
+    'KeyW',
+    'KeyX',
+    'KeyY',
+    'KeyZ',
+  ];
+
+  const handleKeyPress = (e) => {
+    for (let i in keyCodes) {
+      if (e.code === keyCodes[i]) {
+        setKeyCode(`${e.code}, ${e.keyCode}`);
+      }
+    }
+  };
 
   useEffect(() => {
-    document.title = `You clicked ${count} times`;
+    document.addEventListener('keypress', handleKeyPress);
   });
 
   return (
     <div>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <p>{count}</p>
+      <p>{keyCode}</p>
     </div>
   );
 }
