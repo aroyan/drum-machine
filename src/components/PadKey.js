bc1cbf7b9add23e2a22a6c99168455a6dfe1c6ad
Aroyan <abssneakers@gmail.com>
2022-02-18 09:45:12 +0700

    Split DrumPad and PadKey components from App component

---
+++ src/components/PadKey.js
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+
+export default function PadKey(props) {
+  //Handle keypress event and play sound
+  function handleKeyPress(e) {
+    if (
+      e.key === props.keyTrigger.toLowerCase() ||
+      e.key === props.keyTrigger
+    ) {
+      props.setDisplay(props.clipId);
+      playAudio();
+    }
+  }
+  //Play audio
+  function playAudio() {
+    const audio = document.getElementById(props.keyTrigger);
+    audio.currentTime = 0;
+    audio.play();
+    props.setDisplay(props.clipId);
+  }
+  //Add event listener using useEffect hook
+  useEffect(() => {
+    document.addEventListener('keypress', handleKeyPress);
+    //Clean up event listener
+    return () => {
+      document.removeEventListener('keypress', handleKeyPress);
+    };
+  });
+  return (
+    <div
+      className="drum-pad"
+      id={props.clipId}
+      onClick={playAudio}
+      style={{
+        border: '1px solid orange',
+        marginBottom: '8px',
+        padding: '8px',
+      }}
+    >
+      <audio src={props.clip} className="clip" id={props.keyTrigger} />
+      {props.keyTrigger}
+    </div>
+  );
+}
